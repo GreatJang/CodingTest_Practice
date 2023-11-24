c8898cfd920cb3595dd37d9b7e9c4216c9ee0987
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-11-24 17:17:21 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
         if(str1.contains(str2))
+            // contains :
+            // 문자열이 특정 부분 문자열(substring)을 포함하고 있는지 여부를 확인하는 메서드
             return 1;
         else
             return 2;
