12f684290c765adebd44ef26f2f8a0b94c8820e6
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-11-28 19:10:23 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n) {
+        double a = Math.sqrt(n);
+        if(a == (int)a) {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    }
+}
