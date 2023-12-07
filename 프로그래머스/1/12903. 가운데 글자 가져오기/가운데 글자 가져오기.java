3af9a848aed57701b9f736803d38264285a349a6
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-12-07 14:50:07 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -4,7 +4,7 @@ class Solution {
         if(s.length() % 2 == 1) {
             answer = s.substring(s.length() / 2, s.length() / 2 + 1);
         } else {
-            answer =s.substring(s.length() / 2 - 1, (s.length() / 2) + 1);
+            answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
         }
         return answer;
     }
