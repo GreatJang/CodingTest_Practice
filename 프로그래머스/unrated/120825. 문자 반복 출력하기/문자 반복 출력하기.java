4028441c5fbafecc0c1f6678a755dfd25100c065
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-12-07 09:54:43 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 13.54 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        char[] a = new char[my_string.length()];
+            for(int i = 0; i < my_string.length(); i++) {
+                a[i] = my_string.charAt(i);
+                for(int m = 1; m <= n; m++){
+                    answer += a[i];
+                }
+                // answer += a[i];
+                // answer += a[i];
+            }
+        return answer;
+    }
+}
