3342d9b0bb9a8dd71e4bf94c5c8d885c3c72d5f5
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-12-13 16:18:45 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 10.45 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -6,11 +6,10 @@ class Solution {
         char temp = my_char[num1];
         my_char[num1] = my_char[num2];
         my_char[num2] = temp;
+        // 배열을 다시 문자열로 변환 시 for문 돌리는게 빠름.
         for(int i=0; i<my_char.length; i++){
             answer += my_char[i];
         }
         return answer;
-        // String answer = Arrays.toString(my_char);
-        // return answer;
     }
 }
