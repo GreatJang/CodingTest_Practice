e13b43bf99e8c4e3fa09d92864742304d7a53ac8
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-12-16 23:50:35 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.19 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,46 +1,27 @@
-import java.util.*;
 
+import java.util.*;
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        LinkedList<Integer> numberList = new LinkedList<>();
-        for (int num : numbers) {
-            numberList.add(num);
+        LinkedList<Integer> newList = new LinkedList<>();
+        for(int num : numbers){
+            newList.add(num);
         }
-
-        if (direction.equals("right")) {
-            int lastElement = numberList.removeLast();
-            numberList.addFirst(lastElement);
-        } else if (direction.equals("left")) {
-            int firstElement = numberList.removeFirst();
-            numberList.addLast(firstElement);
+        
+        if(direction.equals("right")){
+            newList.add(0,newList.get(newList.size()-1));
+            newList.remove(newList.size()-1);
         }
-
-        return numberList.stream().mapToInt(Integer::intValue).toArray();
+        else if(direction.equals("left")){
+            newList.add(newList.size(), newList.get(0));
+            newList.remove(0);
+        }
+        
+        int[] answer = new int[newList.size()];
+        for (int i = 0; i < newList.size(); i++) {
+            answer[i] = newList.get(i);
+        }
+        return answer;
     }
 }
-
-// import java.util.*;
-// class Solution {
-//     public int[] solution(int[] numbers, String direction) {
-//         List<Integer> newList = new ArrayList<>();
-//         for(int a : numbers){
-//             newList.add(a);
-//         }
-        
-//         if(direction.equals("right")){
-//             newList.add(0,newList.size());
-//             newList.remove(newList.size()-1);
-//         } else if(direction.equals("left")){
-//             newList.add(newList.size(), newList.get(0));
-//             newList.remove(0);
-//         }
-        
-//         int[] answer = new int[newList.size()];
-//         for (int i = 0; i < newList.size(); i++) {
-//             answer[i] = newList.get(i);
-//         }
-//         return answer;
-//     }
-// }
-//String 문자열 비교시 무조건 .equals 쓰기 안써서 정상동작 X
-//else문에 처음에는 if문으로만 사용했는데 오류나서 정상동작X
+// String 문자열 비교시 무조건 .equals 쓰기 안써서 정상동작 X
+// else문에 처음에는 if문으로만 사용했는데 오류나서 정상동작X
