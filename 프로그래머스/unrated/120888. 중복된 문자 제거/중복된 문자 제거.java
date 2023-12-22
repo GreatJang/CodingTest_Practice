ad13dae40e902a6918eca09825d19e40dfe4bf98
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-12-22 11:13:32 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 3.85 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,21 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        String[] a = new String[my_string.length()];
-        for (int i = 0; i < my_string.length(); i++) {
-            a[i] = String.valueOf(my_string.charAt(i));
-        }
-        Set<String> a2 = new LinkedHashSet<>(List.of(a));
-        for (String s : a2) {
-            sb.append(s);
-        }
-        return sb.toString();
-        
+        String[] a = my_string.split("");
+        String[] b = Arrays.stream(a).distinct().toArray(String[]::new);
+        String answer = String.join("",b);
+        return answer;
     }
 }
 
-// // collection으로 해결해보기
+// import java.util.*;
+// class Solution {
+//     public String solution(String my_string) {
+//         String[] a = my_string.split("");
+//         Set<String> a2 = new LinkedHashSet<>(Arrays.asList(a));
+//         return String.join("",a2);
+//     }
+// }
+
 // import java.util.*;
 // class Solution {
 //     public String solution(String my_string) {
@@ -23,10 +24,6 @@ class Solution {
 //         for (int i = 0; i < my_string.length(); i++) {
 //             list.add(String.valueOf(my_string.charAt(i)));
 //         }
-//         // if(list.size() == 1){
-//         // String answer = String.join("", list);
-//         // return answer;
-//         // }
 //         for (int i = 0; i < list.size(); i++) {
 //             for (int j = i + 1; j < list.size(); j++) {
 //                 if (list.get(i).equals(list.get(j))) {
