4fdcb8e310bade111443a0ca09b0857a96476488
GreatJang <148752498+GreatJang@users.noreply.github.com>
2024-01-02 14:23:58 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.11 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int n) {
-        String a = Integer.toString(n,3);
-        char[] arr = a.toCharArray();
+        String a = Integer.toString(n,3); // 10진법을 3진법으로 변환(String)
+        char[] arr = a.toCharArray(); // char 배열에 3진법(String)값 초기화
 
-        for (int i = 0; i < a.length()/2; i++) {
+        for (int i = 0; i < a.length()/2; i++) { // 배열 뒤집기 로직
             char temp = arr[i];
             arr[i] = arr[arr.length - 1 - i];
             arr[arr.length - 1 - i] = temp;
         }
-        String answer = new String(arr);
-        return Integer.parseInt(answer,3);
+        String answer = new String(arr); // 문자배열을 String으로 변환
+        return Integer.parseInt(answer,3); // 3진법을 10진법으로 변환하여 출력
     }
 }
