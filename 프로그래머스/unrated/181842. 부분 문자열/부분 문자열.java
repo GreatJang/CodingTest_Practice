b170150e839352ccd16602a97276e0a9d77f10e2
GreatJang <148752498+GreatJang@users.noreply.github.com>
2024-01-04 17:39:26 +0900

    [level unrated] Title: 부분 문자열, Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(String str1, String str2) {
-        boolean answer = str2.contains(str1);
-        if(answer == true){
-            return 1;
-        }else {
-            return 0;
-        }
+        // boolean answer = str2.contains(str1);
+        return (str2.contains(str1)==true) ? 1 : 0;
+        // if(answer == true){
+        //     return 1;
+        // }else {
+        //     return 0;
+        // }   
     }
 }
