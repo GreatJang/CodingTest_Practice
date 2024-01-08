40bdfb98a1beadc2320c98ed20653d57dde666cc
GreatJang <148752498+GreatJang@users.noreply.github.com>
2024-01-08 19:20:06 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if(my_string.length()<is_prefix.length()){
+            return 0;
+        }
+        String a = my_string.substring(0,is_prefix.length());
+        if(a.equals(is_prefix)){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
