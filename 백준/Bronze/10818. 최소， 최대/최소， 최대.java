31e04bba06aa40e67c256c00031d6e206fb8ec30
GreatJang <148752498+GreatJang@users.noreply.github.com>
2024-06-04 10:25:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 1240 ms, Memory: 105188 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        bw.write(A[0] + " ");
+        bw.write(A[N-1] + " ");
+        bw.flush();
+    }
+}
